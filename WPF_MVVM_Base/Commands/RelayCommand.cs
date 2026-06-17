41ddd10394546d452eec0f7123abe4139b40e9ac
Gordon <bkgr4949p@icloud.com>
2026-06-18 03:06:59 +0900

    feat: RelayCommand 사용 예시 추가
    - ICommand를 구현한 RelayCommand 클래스 추가
    - MainViewModel에 버튼 실행 명령과 실행 가능 조건 추가
    - Button의 Command 바인딩으로 클릭 동작 연결

---
+++ WPF_MVVM_Base/Commands/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPF_MVVM_Base.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+            {
+                return true;
+            }
+
+            return _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+    }
+}
--- WPF_MVVM_Base/ViewModels/MainViewModel.cs
+++ WPF_MVVM_Base/ViewModels/MainViewModel.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_MVVM_Base.Commands;
 
 namespace WPF_MVVM_Base.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        // 버튼 클릭 횟수
+        private int _count;
 
+        public ICommand IncreaseCommand { get; }
+
+        public MainViewModel()
+        {
+            IncreaseCommand = new RelayCommand(Increase, canIncrease);
+        }
+
+        // 버튼 클릭 이벤트 콜백
+        private void Increase()
+        {
+            _count++;
+
+            MessageBox.Show($"Count: {_count}");
+        }
+
+        // 버튼 클릭 가능 여부 체크 콜백
+        private bool canIncrease()
+        {
+            // 버튼 클릭 횟수가 5 미만일 때만 버튼이 활성화되도록 설정
+            return _count < 5;
+        }
     }
 }
